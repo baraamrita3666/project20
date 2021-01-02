29ca5ca70d925c41b716c73d10f5be9abfc6470f
baraamrita3666 <76258063+baraamrita3666@users.noreply.github.com>
2021-01-02 19:20:55 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,73 +1,47 @@
-var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
+var car,wall
 
-function preload()
-{
-	helicopterIMG=loadImage("helicopter.png")
-	packageIMG=loadImage("package.png")
-}
+var speed,weight
+
+
 
 function setup() {
-	createCanvas(800, 700);
-	rectMode(CENTER);
-	
-
-	packageSprite=createSprite(width/2, 80, 10,10);
-	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.2
-
-	helicopterSprite=createSprite(width/2, 200, 10,10);
-	helicopterSprite.addImage(helicopterIMG)
-	helicopterSprite.scale=0.6
-
-	groundSprite=createSprite(width/2, height-35, width,10);
-	groundSprite.shapeColor=color(255)
- 
-
-	box1=createSprite(500,610,20,100);
-	box1.shapeColor=color("red");
-
-	box2=createSprite(300,610,20,100);
-	box2.shapeColor=color("red");
-
-	box3=createSprite(400,650,200,20);
-    box3.shapeColor=color("red")
-	engine = Engine.create();
-	world = engine.world;
-
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
-	World.add(world, packageBody);
-	
-
-	//Create a Ground
-	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
- 	World.add(world, ground);
-	
-	Engine.run(engine);
+  createCanvas(1500,400);
   
+  speed=random(55,90);
+  weight=random(400,1500)
+  
+  
+  
+  car=createSprite(50, 200, 50, 50);
+   
+  car.velocityX = speed;
+
+  car.shapeColor = color(80);
+
+  wall=createSprite(1000,200,60,height/2);
+  wall.shapeColor=color(80,80,80)
 }
 
 
 function draw() {
-  rectMode(CENTER);
-  background(0);
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
+  background(0);  
+  
+  if(wall.x-car.x < (car.width+wall.width)/2)
+  {
+    car.velocityX=0;
+    var deformation=0.5 *weight * speed * speed/22509;
+    if(deformation>180);
+    {
+      car.shapeColor=color(255,0,0)
+    }
+    if(deformation<180 && deformation>100)
+    {
+      car.shapeColor = color(230,230,0)
+    }
+    if(deformation<100)
+    {
+      car.shapeColor = color(0,255,0)
+    }
+}
   drawSprites();
- keyPressed();
 }
-
-function keyPressed() {
- if (keyCode === DOWN_ARROW) {
-	console.log("down");
-	Matter.Body.setStatic(packageBody,false);
-    
-  }
-}
-
-
-
